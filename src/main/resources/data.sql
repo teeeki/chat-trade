94b8dad7ca7ed70e1bd5fc09a4fff4949aa14940
ARI-teieki <tatsuya.teieki@ari-jp.com>
2025-05-30 13:50:09 +0900

    sql読み込みとサーバ起動 done

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO users(username, password) VALUES('t-teieki', 'himitu');
+INSERT INTO users(username, password) VALUES('aaa', 'ccc');
+INSERT INTO users(username, password) VALUES('bbb', 'bbb');
+
+
+
+INSERT INTO items(user_id, name, price) VALUES(1, '教科書A', 500);
+INSERT INTO items(user_id, name, price) VALUES(2, '教科書b', 1000);
+INSERT INTO items(user_id, name, price) VALUES(3, 'マスタリングTCPIP', 3000);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS items;
+
+
+-- アカウントテーブル
+CREATE TABLE users
+(
+	id SERIAL PRIMARY KEY,
+	username TEXT,
+	password TEXT
+);
+
+
+CREATE TABLE items
+(
+	id SERIAL PRIMARY KEY,
+	user_id INTEGER,
+	name TEXT,
+	price INTEGER,
+	img BYTEA
+)
