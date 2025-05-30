41a65d8f6f7bdd7e9ae4b170933b9e7c688e8f5b
ARI-teieki <tatsuya.teieki@ari-jp.com>
2025-05-30 13:42:03 +0900

    first commit

---
+++ src/main/java/com/example/demo/PDev20TeiekiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PDev20TeiekiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PDev20TeiekiApplication.class, args);
+	}
+
+}
